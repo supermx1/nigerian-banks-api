acb21614e721fdecb5e82320823338d3ff678966
super_mx <emeka_toby@yahoo.com>
2023-08-18 22:10:54 +0100

    fixed bug in workflow not committing data.json

--- run.sh
+++ run.sh
@@ -5,10 +5,4 @@ rm *.txt
 timestamp=$(date +"%Y_%m_%d_%I_%M_%p")
 
 # Define the filename with the timestamp
-filename="${timestamp}.txt"
-
-# Create the file
-touch "$filename"
-
-# Print a message
-echo "Created file: $filename"
+touch ${timestamp}.txt
